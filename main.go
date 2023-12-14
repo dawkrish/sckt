73ea07c3f03347ccf0fc808f5b2656ab63448838
krishnanshagarwal112 <krishnansh710@gmail.com>
2023-12-14 14:02:27 +0530

    main

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func main(){
+	r := mux.NewRouter()
+	
+	r.HandleFunc("/",homeHandler)
+	r.HandleFunc("/ws",wsHandler)	
+	
+
+	log.Println("listening on http://localhost:8080")
+	http.ListenAndServe("localhost:8080",r)
+}
