fb00ed9e1b68c44a250597f8aac09de024081161
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-12-15 16:40:28 +0530

    main

--- main.go
+++ main.go
@@ -1,31 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var ClientList []*websocket.Conn
+type DatabaseConfig struct{
+	mongoClient *mongo.Client
+	userColl *mongo.Collection
 
-// type Client struct{
-// 	conn *websocket.Conn
-// }
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
 }
 
-func main(){
+type Room struct{
+	Code int
+	Name string
+	ClientList []*websocket.Conn
+}
+
+var (
+	room Room
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	dbCfg *DatabaseConfig = new(DatabaseConfig)
+)
+
+func main() {
+	conncectToDB()
+	defer dbCfg.mongoClient.Disconnect(context.TODO())
+	getAllUsers()
 	r := mux.NewRouter()
-	
-	r.HandleFunc("/",homeHandler)
-	r.HandleFunc("/ws",wsHandler)	
-	
+
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/ws", wsHandler)
 
 	log.Println("listening on http://localhost:8080")
-	http.ListenAndServe("localhost:8080",r)
+	http.ListenAndServe("localhost:8080", r)
 }
